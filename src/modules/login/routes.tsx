fafcf9c9ce7028189cd476655fe983133ff2735c
Giovanni-LN <giovanni.lima.nascimento@gmail.com>
2025-05-18 10:02:53 -0300

    feat: insert react-router

---
+++ src/modules/login/routes.tsx
@@ -0,0 +1,10 @@
+import type { RouteObject } from 'react-router-dom';
+
+import LoginScreen from './screens/LoginScreen';
+
+export const loginRoutes: RouteObject[] = [
+  {
+    path: '/login',
+    element: <LoginScreen />,
+  },
+];
