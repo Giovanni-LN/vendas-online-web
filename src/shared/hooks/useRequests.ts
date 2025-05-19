58ead263645c59aef168b456335ea49e6e350a81
Giovanni-LN <giovanni.lima.nascimento@gmail.com>
2025-05-19 16:20:36 -0300

    feat: get products

--- src/shared/hooks/useRequests.ts
+++ src/shared/hooks/useRequests.ts
@@ -6,9 +6,7 @@ import { ProductRoutesEnum } from '../../modules/product/routes';
 import { ERROR_INVALID_PASSWORD } from '../constants/errosStatus';
 import { URL_AUTH } from '../constants/urls';
 import { setAuthorizationToken } from '../functions/connection/auth';
-import type {
-  MethodType,
-} from '../functions/connection/connectionAPI';
+import type { MethodType } from '../functions/connection/connectionAPI';
 import ConnectionAPI, {
   connectionAPIPost
 } from '../functions/connection/connectionAPI';
@@ -44,22 +42,6 @@ export const useRequests = () => {
     return returnObject;
   };
 
-  const postRequest = async <T>(url: string, body: unknown): Promise<T | undefined> => {
-    setLoading(true);
-
-    const returnData = await connectionAPIPost<T>(url, body)
-      .then((result) => {
-        return result;
-      })
-      .catch((error: Error) => {
-        setNotification(error.message, 'error');
-        return undefined;
-      });
-
-    setLoading(false);
-    return returnData;
-  };
-
   const authRequest = async (body: unknown): Promise<void> => {
     setLoading(true);
 
@@ -82,6 +64,5 @@ export const useRequests = () => {
     loading,
     authRequest,
     request,
-    postRequest,
   };
 };
