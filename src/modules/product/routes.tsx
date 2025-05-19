062a6a55b26d329821af8e1499e5264027c7f2c7
Giovanni-LN <giovanni.lima.nascimento@gmail.com>
2025-05-19 20:22:14 -0300

    feat: create screen, breadcrumb, product insert

--- src/modules/product/routes.tsx
+++ src/modules/product/routes.tsx
@@ -1,14 +1,20 @@
 import type { RouteObject } from 'react-router-dom';
 
 import Product from './screens/Product';
+import ProductInsert from './screens/ProductInsert';
 
 export const ProductRoutesEnum = {
   PRODUCT: '/product',
+  PRODUCT_INSERT: '/product/insert',
 } as const;
 
 export const productScreens: RouteObject[] = [
   {
     path: ProductRoutesEnum.PRODUCT,
     element: <Product />,
+  },
+  {
+    path: ProductRoutesEnum.PRODUCT_INSERT,
+    element: <ProductInsert />,
   },
 ];
---
+++ src/shared/components/screen/Screen.tsx
@@ -0,0 +1,19 @@
+import type { ListBreadcrumb } from '../breadcrumb/Breadcrumb';
+import Breadcrumb from '../breadcrumb/Breadcrumb';
+import { ScreenContainer } from './screen.style';
+
+interface ScreenProps {
+  children: React.ReactNode;
+  listBreadcrumb?: ListBreadcrumb[];
+}
+
+const Screen = ({ children, listBreadcrumb }: ScreenProps) => {
+  return (
+    <ScreenContainer>
+      {listBreadcrumb && <Breadcrumb listBreadcrumb={listBreadcrumb} />}
+      {children}
+    </ScreenContainer>
+  );
+};
+
+export default Screen;
