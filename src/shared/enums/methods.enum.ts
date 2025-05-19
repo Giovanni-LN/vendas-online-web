b76709c34b3cd3f138f28d027904ee337b5cb0b8
Giovanni-LN <giovanni.lima.nascimento@gmail.com>
2025-05-19 16:00:18 -0300

    feat: refactor requests

--- src/shared/enums/methods.enum.ts
+++ src/shared/enums/methods.enum.ts
@@ -1,9 +1,9 @@
-export const MethodsEnum = {
-  GET: 'GET',
-  POST: 'POST',
-  DELETE: 'DELETE',
-  PATCH: 'PATCH',
-  PUT: 'PUT',
-} as const;
+export type MethodsEnum = 'get' | 'post' | 'delete' | 'patch' | 'put';
 
-export type MethodsEnum = (typeof MethodsEnum)[keyof typeof MethodsEnum];
+export const MethodsEnum = {
+  GET: 'get',
+  POST: 'post',
+  DELETE: 'delete',
+  PATCH: 'patch',
+  PUT: 'put',
+} as const;
