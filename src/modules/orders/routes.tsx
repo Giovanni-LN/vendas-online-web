e32bde770d7f18f6a3c4967da13d784807d95b79
Giovanni-LN <giovanni.lima.nascimento@gmail.com>
2025-05-22 17:03:02 -0300

    feat: create screen order detail

--- src/modules/orders/routes.tsx
+++ src/modules/orders/routes.tsx
@@ -1,14 +1,20 @@
 import type { RouteObject } from 'react-router-dom';
 
 import Order from './screens/Order';
+import OrderDetail from './screens/OrderDetail';
 
 export const OrderRoutesEnum = {
   ORDER: '/order',
+  ORDER_ID: '/order/id',
 } as const;
 
 export const orderScreens: RouteObject[] = [
   {
     path: OrderRoutesEnum.ORDER,
     element: <Order />,
+  },
+  {
+    path: OrderRoutesEnum.ORDER_ID,
+    element: <OrderDetail />,
   },
 ];
